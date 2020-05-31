856229b6e550c0bab3459e5bc6eef0a030f9b226
shifty3y3s <44274185+shifty3y3s@users.noreply.github.com>
2020-05-31 01:44:38 -0500

    Edits to fixScale()
    Fix scale now has a default lower bound of 0 and upper bound of 255. Makes code prettier

--- Python/pixelMethods.py
+++ Python/pixelMethods.py
@@ -6,7 +6,7 @@ from PIL import Image
 #Apparently range is supposed to be switched with prange if you add njit though
 
 @njit()
-def fixScale(low, high, value): # Verifies a value falls within a range, e.g. 0-255.
+def fixScale(value, low=0, high=255): # Verifies a value falls within a range, e.g. 0-255.
 	if(value<low): return low   # Rounds if above or below range
 	if(value>high): return high
 	return int(value)
@@ -17,16 +17,16 @@ def dummy(pixel, amount): # Dummy function to test runtimes
 
 @njit()
 def brightness(pixel, amount): # applies the brightness effect. Looks ugly but it ran faster than the prettier code.
-	return (fixScale(0,255,pixel[0]+amount),fixScale(0,255,pixel[1]+amount),fixScale(0,255,pixel[2]+amount)) 
+	return (fixScale(pixel[0]+amount),fixScale(pixel[1]+amount),fixScale(pixel[2]+amount)) 
 
 
 @njit()
 def contrast(pixel, factor): # Contrast effect. Looks super ugly, but for whatever reasons runs faster.
-	return (fixScale(0,255,factor * ((pixel[0] - 128) + 128)),fixScale(0,255,factor * ((pixel[1] - 128) + 128)),fixScale(0,255,factor * ((pixel[2] - 128) + 128)))
+	return (fixScale(factor * ((pixel[0] - 128) + 128)),fixScale(factor * ((pixel[1] - 128) + 128)),fixScale(factor * ((pixel[2] - 128) + 128)))
 
 @njit()
 def exposure(p, ev):
-	return (fixScale(0,255, p[0] + invert(p[0])*ev), fixScale(0,255, p[1] + invert(p[1])*ev), fixScale(0,255, p[2] + invert(p[2])*ev))
+	return (fixScale(p[0] + invert(p[0])*ev), fixScale(p[1] + invert(p[1])*ev), fixScale(p[2] + invert(p[2])*ev))
 
 @njit()
 def invert(value, high=255):
